1b2d0f45527c924974afb53240dcf064a5e081ad
Oleh Tsap <olehtsap07@gmail.com>
2022-09-22 21:22:46 +0300

    start js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// const shouldRenew = confirm('Бажаєте продовжити підписку');
+// console.log(shouldRenew);
+// let quantity = prompt('Введіть текст');
+// console.log(quantity);
+// quantity = Number(quantity);
+// console.log(typeof quantity);
