868380433779eb3207d79e5fe35944538c55a667
Oleh Tsap <olehtsap07@gmail.com>
2022-09-26 17:09:37 +0300

    фікс домашки, виконана 8 задача з 2 частини

--- js/index.js
+++ js/index.js
@@ -4,27 +4,50 @@
 // console.log(quantity);
 // quantity = Number(quantity);
 // console.log(typeof quantity);
+
+
+// Частина 1
+
+// Завдання 1 і 2 разом
 const str = 'Привіт';
-console.log(str);
-console.log(typeof str);
+// console.log(str);
+// console.log(typeof str);
 const num = 123;
-console.log(num);
-console.log(typeof num);
+// console.log(num);
+// console.log(typeof num);
 const flag = true;
-console.log(flag);
-console.log(typeof flag);
+// console.log(flag);
+// console.log(typeof flag);
 const txt = 'true';
-console.log(txt);
-console.log(typeof txt);
-const quantity = prompt("Ім'я користувача");
-console.log(quantity);
-let atest = prompt('Якого року був початок Першої світової війни?');
-console.log(atest);
-let btest = prompt("Якого року відбулася Галицька битва?");
-console.log(btest);
-let ctest = prompt("Якого року відбувся Горлицький прорив?");
-console.log(ctest);
-let dtest = prompt("Якого року відбулася битва на горі Маківка?");
-console.log(dtest);
-let etest = prompt("Де і якого року відбулося створення ЗУР?");
-console.log(etest); 
+// console.log(txt);
+// console.log(typeof txt);
+// const site = confirm('Ви зайшли на сайт по JS');
+// console.log(site);
+// let atest = prompt('Якого року був початок Першої світової війни?');
+// console.log(atest);
+// let btest = prompt("Якого року відбулася Галицька битва?");
+// console.log(btest);
+// let ctest = prompt("Якого року відбувся Горлицький прорив?");
+// console.log(ctest);
+// let dtest = prompt("Якого року відбулася битва на горі Маківка?");
+// console.log(dtest);
+// let etest = prompt("Де і якого року відбулося створення ЗУР?");
+// console.log(etest);
+
+
+// Частина 2
+
+// Завдання 1
+// const quantity = prompt("Ім'я користувача");
+// console.log(quantity);
+// Завдання 2
+// Завдання 3
+// Завдання 4
+// Завдання 5
+// Завдання 6
+// Завдання 7
+// Завдання 8
+let n = 3;
+let m = 4;
+const k = (n**2) + (m**2);
+// console.log(k); 
