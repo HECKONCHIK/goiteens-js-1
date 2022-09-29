dfe27b34e7bf0503c62bcf15e4467a986fe16dc0
Oleh Tsap <olehtsap07@gmail.com>
2022-09-29 19:45:50 +0300

    практичне заняття

---
+++ js/practice.js
@@ -0,0 +1,60 @@
+// 2. Створити рядок довільного змісту. Знайти довжину цього рядка.
+    // Змінити його зміст і знайти індекс останньої і першої літери!
+// let str = 'Олег вчиться писати JS.';
+// console.log(str.length);
+// str = 'Олег продовжує вчитись писати JS.';
+// console.log(str.indexOf('О'));
+// console.log(str.indexOf('.'));
+
+// 3. Написати скрипт який виведе рядок в форматі
+    // "Студент a b записався в школу d на курси i в групу h"
+    // підставивши замість a b d i h значення змінних.
+// const firstName = 'Олег';
+// const lastName = 'Цап';
+// const school = 'Goiteens';
+// const group = '7';
+// console.log(`Студент ${firstName} ${lastName} записався в школу ${school} на курси і групу ${group}.`);
+
+// 4. Зміни ім"я товара на "Repair droid" і збільшіть його ціну
+    // на 1500 кредитів. Переопреділіть значення змінних pricePerItem і productName
+    // після їх об"явлення.
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// productName = 'Repair droid';
+// pricePerItem = 3500;
+
+// 5. Об"явіть змінні використавуючи ключові слова const та let та присвойте
+    // їм відповідні значення.
+    // topSpeed - число 160.
+    // distance - число 617.54.
+    // login - рядок "mango935".
+    // isOnline - буль true.
+    // isAdmin - буль false.
+// let topSpeed = 160;
+// let distance = 617.54;
+// const login = 'mango935';
+// const isOnline = true;
+// const isAdmin = false;
+
+// 6. Доповни код, надавши змінній totalPrice вираз для підрахунку загальної суми 
+    // замовлення. Змінна pricePerItem зберігає ціну однієї одиниці товару, 
+    // а orderedQuantity - кількість одиниць товару в замовлення.
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);   
+
+// 12. Створіть змінну яка буде генерувати псевдовипадкове число від 0 до 1 та зміни діапазон від 50 до 100.
+// let randomNumber = Math.random();
+// randomNumber = Math.random() * (100 - 50) + 50;
+// console.log(Math.round(randomNumber));
+
+// 11. Напиши скрип який буде збільшувати заробітню платню робітника на 10%. Початкова сума зп - 25000.589612 грн. Результат округли до сотої та виведи в консоль.
+
+// const sallary = 25000.589612;
+// const newSallary = (sallary/100*10) + sallary;
+// console.log(newSallary.toFixed(2));
+
+// 9. Ціна 1кг м"яса 200грн. Напиши скрипт який просить користувача ввести кількість м"яса в замовлені в кг. 
+    // Порахуй загальну вартість замовлення та виведи результат користувачу.
+
