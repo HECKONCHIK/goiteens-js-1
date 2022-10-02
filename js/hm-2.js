fc857a4ee7675e7beec31f77d1ea26589e617f6a
Oleh Tsap <olehtsap07@gmail.com>
2022-10-02 17:13:32 +0300

    д/з №2

---
+++ js/hm-2.js
@@ -0,0 +1,46 @@
+// Завдання №1
+const k1 = '5px';
+const k2 = '12djd334';
+const k3 = '12.45asdwe12';
+const k4 = 'qwqweeewq';
+// console.log(Number.parseInt(k1));
+// console.log(Number.parseInt(k2));
+// console.log(Number.parseInt(k3));
+// console.log(Number.parseInt(k4));
+
+// Завдання №2
+// console.log(Number.parseFloat(k1));
+// console.log(Number.parseFloat(k2));
+// console.log(Number.parseFloat(k3));
+// console.log(Number.parseFloat(k4));
+
+// Завдання №3
+// console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+// console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+
+// Завдання №4
+const random = Math.random() * (20 - 3) + 3;
+// console.log(Number.parseInt(random));
+
+// Завдання №5
+const result = 5 + 5 + '5';
+// console.log(result);
+
+// Завдання №6
+const email = 'olehtsap07@gmail.com';
+const mailDog = '@';
+// console.log(email.length);
+// console.log(email.includes(mailDog));
+
+// Завдання №7
+const my = 'My';
+const name = 'name';
+const is = 'is';
+const fullName = `${ my } ${ name } ${ is } Viktor`;
+// console.log(fullName);
+
+// Завдання №8
+const userName = 'Олег';
+const peyment = '3499';
+const userAlert = `Дякуємо, ${userName}! До сплати ${peyment} гривень`;
+// alert(userAlert);
