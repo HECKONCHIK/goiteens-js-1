ef2ea1b39e6916be62b961da3ec41815f8fbe470
Oleh Tsap <olehtsap07@gmail.com>
2022-10-03 16:57:08 +0300

    фікс дз

--- js/hm-2.js
+++ js/hm-2.js
@@ -1,18 +1,22 @@
 // Завдання №1
-const k1 = '5px';
-const k2 = '12djd334';
-const k3 = '12.45asdwe12';
-const k4 = 'qwqweeewq';
-// console.log(Number.parseInt(k1));
-// console.log(Number.parseInt(k2));
-// console.log(Number.parseInt(k3));
-// console.log(Number.parseInt(k4));
+const k1 = Number.parseInt('5px');
+const k2 = Number.parseInt('12djd334');
+const k3 = Number.parseInt('12.45asdwe12');
+const k4 = Number.parseInt('qwqweeewq');
+// console.log(k1);
+// console.log(k2);
+// console.log(k3);
+// console.log(k4);
 
 // Завдання №2
-// console.log(Number.parseFloat(k1));
-// console.log(Number.parseFloat(k2));
-// console.log(Number.parseFloat(k3));
-// console.log(Number.parseFloat(k4));
+const k5 = Number.parseFloat('5px');
+const k6 = Number.parseFloat('12djd334');
+const k7 = Number.parseFloat('12.45asdwe12');
+const k8 = Number.parseFloat('qwqweeewq');
+// console.log(k1);
+// console.log(k2);
+// console.log(k3);
+// console.log(k4);
 
 // Завдання №3
 // console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
@@ -24,7 +28,7 @@ const random = Math.random() * (20 - 3) + 3;
 
 // Завдання №5
 const result = 5 + 5 + '5';
-// console.log(result);
+// console.log(typeOf result);
 
 // Завдання №6
 const email = 'olehtsap07@gmail.com';
--- js/practice.js
+++ js/practice.js
@@ -57,6 +57,19 @@
 
 // 9. Ціна 1кг м"яса 200грн. Напиши скрипт який просить користувача ввести кількість м"яса в замовлені в кг. 
     // Порахуй загальну вартість замовлення та виведи результат користувачу.
-// const weight = prompt('Скільки кіло хочете взяти?');
-// const sum = 200 * weight;
-// console.log(sum);
+let pricePerKilo = '200grn';
+pricePerKilo = Number.parseInt(pricePerKilo);
+let orderedQuantity = prompt('Скільки кіло хочете взяти?');
+orderedQuantity = Number.parseFloat(orderedQuantity);
+const sum = pricePerKilo * orderedQuantity;
+alert((sum));
+
+// 13. Напиши скрипт який знайде довжину рядка "Hello, world! Javascript is awesome!".
+    // Зробить всі літери великими. Знайде в рядку
+    // слово "javascript". Всі маніпуляції не повинні змінювати оригінальний рядок.
+    
+// const str = "Hello, world! Javascript is awesome!";
+// const upStr = str.toUpperCase();
+
+// const search = str.includes('javascript');
+// console.log(search);
