a840bd1a8da341966fdaf81eca0d603dd92d3cc0
Oleh Tsap <olehtsap07@gmail.com>
2022-10-11 15:44:10 +0300

    д/з №3

---
+++ js/hm-3.js
@@ -0,0 +1,71 @@
+// Завдання №1
+// let number;
+// const whichNumber = prompt('Вибери число від 0 до 100');
+// // const range = Math.random() * (99 - 54) + 54;
+// if (whichNumber >= 54 && whichNumber <= 99) {
+//     number = "Число потрапляє в діапазон";
+// } else {
+//     number = "Число не потрапляє в діапазон";
+// }
+// console.log(number);
+
+// Завдання №2
+// let age;
+// const maxAge = 55;
+// if (maxAge >= 0 && maxAge <= 16) {
+//     age = 'kids';
+// } else if (maxAge >= 17 && maxAge <= 60) {
+//     age = 'adults';
+// } else if (maxAge >= 61 && maxAge <= 100) {
+//     age = 'pensioners';
+// }
+// console.log(age);
+// alert(age);
+// Завдання №3
+// let firstName = 'Олег';
+// const lengthFirstName = firstName.length;
+// let lastName = 'Цаппр';
+// const lengthLastName = lastName.length;
+// const sum = firstName.length + lastName.length;
+// if (firstName.length >= 4 && lastName.length >= 5) {
+//     alert(`${firstName} - ${lengthFirstName} літер. ${lastName} - ${lengthLastName} літер.`);
+
+//     console.log(sum);
+// } else {
+//     console.log('УПС');
+// }
+// Завдання №4
+// const random = Math.random() * (6 - 0) + 0;
+// console.log(Number.parseInt(random));
+// if (random > 0 && random <= 1) {
+//     console.log(1);
+// } else if (random >= 2 && random < 3) {
+//     console.log(2);
+// } else if (random >= 3 && random < 4) {
+//     console.log(3);
+// } else if (random >= 4 && random < 5) {
+//     console.log(4);
+// } else if (random >= 5 && random < 6) {
+//     console.log(5);
+// }
+// Завдання №5
+// let month
+// let lang = 'en';
+// switch (lang) {
+//     case 'ua':
+//         month = 'Грудень';
+//         break;
+    
+//     case 'ru':
+//         month = 'Декабрь';
+//         break;
+    
+//     case 'fr':
+//         month = 'Décembre';
+//         break;
+    
+//     case 'en':
+//         month = 'December';
+//         break;
+// }
+// console.log(month);
