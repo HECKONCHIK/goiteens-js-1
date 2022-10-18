7673213e303a95ed83efa05684ca6d324ff4022c
Oleh Tsap <olehtsap07@gmail.com>
2022-10-18 18:16:55 +0300

    дз №4

---
+++ js/hm-4.js
@@ -0,0 +1,36 @@
+// Завдання №1
+// const max = 10;
+// for (let i = 0; i <= max; i += 1 ) {
+//     console.log(i);
+// }
+
+// Завдання №2
+// const number = 11;
+
+// for (let i = 0; i < number; i += 1) {
+//     if (i % 2 === 0) {
+//       console.log('Число парне: ', i);
+//     } else {
+//         console.log('Число непарне: ', i);
+//     }
+// }
+
+// Завдання №3
+// let num = 20;
+
+// while (num < 28) {
+//     num += 1;
+//     console.log(num);
+// }
+
+// Завдання №4
+let names = '';
+
+do {
+    names = prompt('Введіть ім’я свого улюбленого героя');
+    console.log(names);
+    
+} while (names.length < 6)
+
+
+
