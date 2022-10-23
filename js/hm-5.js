22254c31f85653755dea77c8660aaeb8227a814e
Oleh Tsap <olehtsap07@gmail.com>
2022-10-23 16:18:07 +0300

    5/6 завдань виконано

---
+++ js/hm-5.js
@@ -0,0 +1,35 @@
+// Завдання №1 
+const Arr1 = [1, 5, '4', 'hello'];
+const Arr2 = [true, 2, {}, ['a'], 222];
+// console.log(Arr1[2] + Arr2[2]);
+
+// Завдання №2
+Arr1[4] = 22;
+// console.table(Arr1);
+
+// Завдання №3
+for (const arr1 of Arr1) {
+    // console.log(typeof arr1);
+}
+
+// Завдання №4
+const message = 'Welcome to Ukraine!'
+
+// console.log(message.split(''));
+// console.log(message.indexOf('l'));
+
+// Завдання №5
+const styles = ['Джаз', 'Блюз'];
+// styles.push('Рок-н-ролл');
+// console.log(styles);
+
+// styles.splice(1, 0, 'Класика');
+// console.log(styles);
+
+// console.log(styles.shift());
+// console.log(styles);
+
+// styles.unshift('Реп', 'Реггі');
+// console.log(styles);
+
+// Завдання №6
