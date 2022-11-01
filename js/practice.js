c35ebbaeb21d83951967660c78a046e6f31c9fd7
Oleh Tsap <olehtsap07@gmail.com>
2022-11-01 20:06:39 +0200

    практичне заняття, та 50% дз №6

--- js/practice.js
+++ js/practice.js
@@ -129,8 +129,8 @@
  * - Метод includes() с тернарним оператором
  */
 
-const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
-const loginToFind = 'aj4xth3m4n';
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+// const loginToFind = 'aj4xth3m4n';
 // let message = `Користувач ${loginToFind} не знайдено`;
 
 // for (let i = 0; i < logins.length; i++ ) {
@@ -302,3 +302,84 @@ const loginToFind = 'aj4xth3m4n';
   /*
    * Оновлення (по індексу)
    */
+
+
+
+
+/*
+ * Напиши функцію calculateTotalPrice(items)
+ * яка приймає масив цін і повертає їх сумму
+ */
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// let total = 0;
+
+// for (const value of cart) {
+//   total += value;
+// }
+
+// console.log('Total: ', total);
+
+// const calculateTotalPrice = function (items) {
+//     console.log(items);
+
+//     let total = 0;
+
+//     for (const item of items) {
+//         total += item;
+//     }
+//     return total;
+// }
+
+
+  
+//   console.log(calculateTotalPrice([1, 2, 3])); // 6
+//   console.log(calculateTotalPrice([5, 10, 15, 20])); // 50
+//   console.log(calculateTotalPrice([100, 200, 300])); // 600
+
+
+/*
+ * Напиши функцію logItems(items) для перебора і логування массива
+ */
+
+// const logItems = function (items) {
+//     for (const item of items) {
+//         console.log(item);
+//     }
+// }
+
+  
+//   logItems(['Mango', 'Kiwi', 'Poly', 'Ajax']);
+//   logItems([1, 2, 3, 4, 5]);
+//   logItems(['клавиатура', 'наушники', 'часы']);
+
+
+
+
+/*
+ * Напиши скрипт пошуку логіна
+ * - Якщо логіна немає, вивести повідомлення 'Користувач [логін] не знайдено.'
+ * - Якщо знайшли логін, вивести повідомлення 'Користувач [логін] знайдено.'
+ */
+
+const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+
+const loginToFind = 'aj4xth3m4n';
+
+const message = logins.includes(loginToFind) 
+// ? `Користувач ${loginToFind} знайдено.` 
+// : `Користувач ${loginToFind} не знайдено.`;
+console.log(message);
+
+const findLogin = function (loginsArray, loginsToFind) {
+    const message = loginsArray.includes(loginToFind) 
+? `Користувач ${loginToFind} знайдено.` 
+: `Користувач ${loginToFind} не знайдено.`;
+    return message;
+}
+
+console.log(findLogin(logins, 'avocod3r'));
+console.log(findLogin(logins, 'avocod3r'));
+console.log(findLogin(logins, 'k1widab3st'));
+console.log(findLogin(logins, 'jam4l'));
+console.log(findLogin(logins, 'poly1scute'));
