5c25994b2cce193de331d938c0903dbfa970b9bc
Oleh Tsap <olehtsap07@gmail.com>
2022-11-19 21:47:41 +0200

    половина дз №8

---
+++ js/hm-8.js
@@ -0,0 +1,103 @@
+// Завдання №1
+
+// const user = {
+//   name: 'Mango',
+//   age: 20,
+//   hobby: 'html',
+//   premium: true,
+// };
+// user.mood = 'happy';
+// user.hobby = 'skidiving';
+// user.premium = false;
+
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//     console.log(key);
+// }
+// Завдання №2
+
+// const countProps = function (obj) {
+//     const values = Object.values(obj);
+//     return values.length;
+// };
+
+// console.log(countProps({})); // 0
+
+// console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+// Завдання №3
+
+// const findBestEmployee = function (employees) {
+//     const entries = Object.entries(employees);
+//     for (const entry of entries) {
+//         const name = entry[0];
+//         const doneTask = entry[1];
+
+//         return
+//     }
+// };
+
+
+// console.log(
+//   findBestEmployee({
+//     ann: 29,
+//     david: 35,
+//     helen: 1,
+//     lorence: 99,
+//   }),
+// ); // lorence
+
+// console.log(
+//   findBestEmployee({
+//     poly: 12,
+//     mango: 17,
+//     ajax: 4,
+//   }),
+// ); // mango
+
+// console.log(
+//   findBestEmployee({
+//     lux: 147,
+//     david: 21,
+//     kiwi: 19,
+//     chelsy: 38,
+//   }),
+// ); // lux
+// Завдання №4
+
+// const countTotalSalary = function (employees) {
+//     const values = Object.values(employees);
+//     let totalSallary = 0;
+//     for (const value of values) {
+//         totalSallary += value;
+//     }
+//     return totalSallary;
+// };
+
+/*
+ * Викличи функції для перевірки працездатності твоєї реалізації.
+ */
+// console.log(countTotalSalary({})); // 0
+
+// console.log(
+//   countTotalSalary({
+//     mango: 100,
+//     poly: 150,
+//     alfred: 80,
+//   }),
+// ); // 330
+
+// console.log(
+//   countTotalSalary({
+//     kiwi: 200,
+//     lux: 50,
+//     chelsy: 150,
+//   }),
+// ); // 400
+// Завдання №5
+
+
+// ЗАвдання №6
+
+// Завдання №7
